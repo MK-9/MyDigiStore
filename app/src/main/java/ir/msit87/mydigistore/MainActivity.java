bcc1d2053a47e995eb30f3a8f3a3cf4f02bf5ffd
Mohammad Soltanmohammadi <msit26055@gmail.com>
2017-09-25 22:47:25 +0330

    Add Font & Animation Helper.

--- app/src/main/java/ir/msit87/mydigistore/MainActivity.java
+++ app/src/main/java/ir/msit87/mydigistore/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page_zf);
+        setContentView(R.layout.activity_near_restaurant);
     }
 }
